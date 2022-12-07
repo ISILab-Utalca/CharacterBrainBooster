82cee3fb38f965c72a9e1e632debb34b267485f5
pokelocos <nare.rare.fire@gmail.com>
2022-12-07 08:57:41 -0300

    Update CFCALIV.cs
    set obsolete

--- CBB-Game/Assets/CFCALIV.cs
+++ CBB-Game/Assets/CFCALIV.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+[Obsolete]
 public class CFCALIV // hace lo que la interfaz
 {
     public List<Utility> GetUtilities(_Agent agent)
