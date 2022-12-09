175700a11489b1e69edc772622978b54ebf12323
pokelocos <nare.rare.fire@gmail.com>
2022-12-09 01:55:16 -0300

    Update Curve.cs

--- CBB-Game/Assets/ISILab/Curve.cs
+++ CBB-Game/Assets/ISILab/Curve.cs
@@ -1,10 +1,12 @@
 using CBB.Api;
+using Newtonsoft.Json;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+[System.Serializable]
 public abstract class Curve
 {
     public abstract float Calc(params float[] parms);
@@ -19,14 +21,31 @@ public abstract class Curve
 
         return exporters.ToList();
     }
+
+    public static List<Vector2> CalcPoints(Curve curve, int steps)
+    {
+        var points = new List<Vector2>();
+        for (int i = 0; i < steps +1; i++)
+        {
+            var x = (1f / (float)steps) * i;
+            var y = curve.Calc(x);
+            points.Add(new Vector2(x, y));
+        }
+        return points;
+    }
 }
 
 [Curve(name:"Linear", "Slope", "Dx", "Dy")]
+[System.Serializable]
 public class Linear : Curve
 {
+    [JsonRequired]
     float value = 0f;
+    [JsonRequired]
     float m = 1f;
+    [JsonRequired]
     float dx = 0f;
+    [JsonRequired]
     float dy = 0f;
 
     public override float Calc(params float[] parms)
@@ -44,14 +63,24 @@ public class Linear : Curve
         value = v;
         return (m * (value + dx)) + dy;
     }
+
+    public override int GetHashCode()
+    {
+        return (int)(Utils.StringToInt(GetType().ToString()) + value * 10 + m * 100 + dx * 1000 + dy * 10000);
+    }
 }
 
 [Curve(name:"Inverted exponential", "e", "Dx", "Dy")]
+[System.Serializable]
 public class ExponencialInvertida : Curve
 {
+    [JsonRequired]
     float value = 0f;   // X
+    [JsonRequired]
     float e = 2f;       // 2f
+    [JsonRequired]
     float dx = 0f;      // 0.0f
+    [JsonRequired]
     float dy = 0f;      // 0.0f
 
     public override float Calc(params float[] parms)
@@ -68,15 +97,25 @@ public class ExponencialInvertida : Curve
     {
         value = v;
         return (1f / (Mathf.Pow(value, e) + dx)) + dy;
+    }
+
+    public override int GetHashCode()
+    {
+        return (int)(Utils.StringToInt(GetType().ToString()) + value * 10 + e * 100 + dx * 1000 + dy * 10000);
     }
 }
 
 [Curve(name: "Exponential", "e", "Dx", "Dy")]
+[System.Serializable]
 public class Exponencial : Curve
 {
+    [JsonRequired]
     float value = 0f;   // X
+    [JsonRequired]
     float e = 2f;       // 2f
+    [JsonRequired]
     float dx = 0f;      // 0.0f
+    [JsonRequired]
     float dy = 0f;      // 0.0f
 
     public override float Calc(params float[] parms)
@@ -94,14 +133,24 @@ public class Exponencial : Curve
         value = v;
         return Mathf.Pow(value + dx, e) + dy;
     }
+
+    public override int GetHashCode()
+    {
+        return (int)(Utils.StringToInt(GetType().ToString()) + value * 10 + e * 100 + dx * 1000 + dy * 10000);
+    }
 }
 
 [Curve(name:"Staggered", "e", "Max", "Min")]
+[System.Serializable]
 public class Escalonada : Curve
 {
+    [JsonRequired]
     float value = 0f;   // X
+    [JsonRequired]
     float e = 0.5f;     // 0.5f
+    [JsonRequired]
     float max = 1f;     // 1f
+    [JsonRequired]
     float min = 0.1f;   // 0.1f
 
     public override float Calc(params float[] parms)
@@ -119,16 +168,27 @@ public class Escalonada : Curve
         value = v;
         return value >= e ? max : min;
     }
+
+    public override int GetHashCode()
+    {
+        return (int)(Utils.StringToInt(GetType().ToString()) + value * 10 + e * 100 + max * 1000 + min * 10000);
+    }
 }
 
 [Curve(name:"Sigmoide", "De", "Dx", "Dy")] // ,"Euler")]
+[System.Serializable]
 public class Sigmoide : Curve
 {
+    [JsonRequired]
     float value = 0f;   // X
+    [JsonRequired]
     float de = 0f;      // 0.0f
+    [JsonRequired]
     float dx = 0f;      // 0.0f
+    [JsonRequired]
     float dy = 0f;      // 0.0f
-    float euler = 2.71828f;   // 2.71828f
+    [JsonIgnore]
+    readonly float euler = 2.71828f;   // 2.71828f
 
     public override float Calc(params float[] parms)
     {
@@ -144,6 +204,11 @@ public class Sigmoide : Curve
     {
         value = v;
         return (1 / (1 + Mathf.Pow(euler + dx, -value + de))) + dy;
+    }
+
+    public override int GetHashCode()
+    {
+        return (int)(Utils.StringToInt(GetType().ToString()) + value * 10 + de * 100 + dx * 1000 + dy * 10000);
     }
 }
 
