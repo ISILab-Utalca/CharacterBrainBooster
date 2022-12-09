b80aa310bc9bb19fd161c45cc4b529e9697613d1
pokelocos <nare.rare.fire@gmail.com>
2022-12-09 01:56:10 -0300

    Update UtilityEvaluators.cs

--- CBB-Game/Assets/ISILab/UtilityEvaluators.cs
+++ CBB-Game/Assets/ISILab/UtilityEvaluators.cs
@@ -22,24 +22,7 @@ namespace CBB.Lib
         }
     }
 
-    [Evaluator("Value","B","C","D")]
-    public class Valentia : UtilityEvaluator // (!) este es el unico complejo deberia estar aqui?
-    {
-        public override float Evaluate(params object[] param)
-        {
-            var parm = param.Select(p => (float)p).ToArray();
-
-            if (param.Length != 4)
-                throw new ArgumentException();
-
-            var dif = parm[2] - parm[3]; // max - min
-            var v1 = (parm[0] - parm[3]) / dif * 1f;
-            var v2 = (parm[1] - parm[3]) / dif * 1f;
-            return ((v1 - v2) + 0.5f) / 2f;
-        }
-    }
-
-    [Evaluator("Value","Min","Max")]
+    [Evaluator(name: "Normalize", "Value", "Min", "Max")]
     public class Normalize : UtilityEvaluator
     {
         public override float Evaluate(params object[] param)
@@ -55,7 +38,7 @@ namespace CBB.Lib
         }
     }
 
-    [Evaluator("Multiplier","Multiplicand")]
+    [Evaluator(name: "Multiply", "Multiplier", "Multiplicand")]
     public class Multiply : UtilityEvaluator
     {
         public override float Evaluate(params object[] param)
@@ -70,7 +53,7 @@ namespace CBB.Lib
         }
     }
 
-    [Evaluator("Dividend","Divisor")]
+    [Evaluator(name: "Divide", "Dividend","Divisor")]
     public class Divide : UtilityEvaluator
     {
         public override float Evaluate(params object[] param)
@@ -85,7 +68,7 @@ namespace CBB.Lib
         }
     }
 
-    [Evaluator("Value")]
+    [Evaluator(name: "Identity", "Value")]
     public class Identity : UtilityEvaluator
     {
         public override float Evaluate(params object[] param)
@@ -97,4 +80,23 @@ namespace CBB.Lib
             return parm[0];
         }
     }
+
+    /*
+   [Evaluator("Value","B","C","D")]
+   public class Valentia : UtilityEvaluator // (!) este es el unico complejo deberia estar aqui?
+   {
+       public override float Evaluate(params object[] param)
+       {
+           var parm = param.Select(p => (float)p).ToArray();
+
+           if (param.Length != 4)
+               throw new ArgumentException();
+
+           var dif = parm[2] - parm[3]; // max - min
+           var v1 = (parm[0] - parm[3]) / dif * 1f;
+           var v2 = (parm[1] - parm[3]) / dif * 1f;
+           return ((v1 - v2) + 0.5f) / 2f;
+       }
+   }
+   */
 }
