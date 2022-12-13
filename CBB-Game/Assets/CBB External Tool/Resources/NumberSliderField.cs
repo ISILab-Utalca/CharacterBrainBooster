e17dfbfb2cafd7bed8a813f3373507c6e826a95e
pokelocos <nare.rare.fire@gmail.com>
2022-12-13 18:01:20 -0300

    Add NumberSliderField class

---
+++ CBB-Game/Assets/CBB External Tool/Resources/NumberSliderField.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class NumberSliderField : VisualElement
+{
+    public FloatField floatField;
+    public Slider slider;
+
+    public NumberSliderField(string name,float value, float min = 0f, float max = 1f, Action<float> OnValueChange = null)
+    {
+        var vt = Resources.Load<VisualTreeAsset>("NumberSliderField");
+        vt.CloneTree(this);
+
+        // FloatField
+        this.floatField = this.Q<FloatField>();
+        this.floatField.label = name;
+        this.floatField.value = value;
+        this.floatField.RegisterCallback<ChangeEvent<float>>(e => {
+            this.slider.value = e.newValue;
+            OnValueChange?.Invoke(e.newValue);
+        });
+
+        // Slider
+        this.slider = this.Q<Slider>();
+        this.slider.lowValue = min;
+        this.slider.highValue = max;
+        this.slider.value = value;
+        this.slider.RegisterCallback<ChangeEvent<float>>(e => {
+            this.floatField.value = e.newValue;
+            OnValueChange?.Invoke(e.newValue);
+        });
+    }
+}
