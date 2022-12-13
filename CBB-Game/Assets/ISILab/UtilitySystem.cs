988d3c1db1106e267e59e84feebe33044838afc7
pokelocos <nare.rare.fire@gmail.com>
2022-12-13 18:02:03 -0300

    Update UtilitySystem.cs

--- CBB-Game/Assets/ISILab/UtilitySystem.cs
+++ CBB-Game/Assets/ISILab/UtilitySystem.cs
@@ -1,22 +1,116 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 
 namespace CBB.Api
 {
-    public class UtilitySystem : MonoBehaviour
+
+    public static class UtilitySystem
     {
-        private void Update()
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void OnBeforeSceneLoadRuntimeMethod()
         {
-            var agents = AgentObserver.Instance.Agents;
-            for (int i = 0; i < agents.Count; i++)
+            Debug.Log("Before scene loaded");
+            CollectAgentBaseData();
+        }
+
+        public static AgentBaseData[] CollectAgentBaseData()
+        {
+            var agentTypes = UtilitySystem.CollectAgentTypes();
+            var agents = new List<AgentBaseData>();
+            foreach (var type in agentTypes)
             {
-                if (agents[i].IsAvailable())
+                var inputs = UtilitySystem.CollectVariables(type);
+                var actions = UtilitySystem.CollectActions(type);
+
+                agents.Add(new AgentBaseData(inputs.ToList(), actions.ToList()));
+            }
+
+            return agents.ToArray();
+        }
+
+        public static Type[] CollectAgentTypes()
+        {
+            var types = Assembly.GetExecutingAssembly()
+                           .GetTypes()
+                           .Where(t => t.GetCustomAttributes(typeof(UtilityAgentAttribute), false).Length > 0)
+                           .ToArray();
+            return types;
+        }
+
+        public static Variable[] CollectVariables(Type typeOwner)
+        {
+            var inputs = new List<Variable>();
+            var fields = typeOwner.GetFields();
+            foreach (var field in fields)
+            {
+                var atts = field.GetCustomAttributes();
+                if (atts.Any(a => a.GetType() == typeof(UtilityInputAttribute)))
                 {
-                    //var action = agents.GetAction();
-                    //action?.invoke();
+                    var att = field.GetCustomAttributes(typeof(UtilityInputAttribute), false)[0] as UtilityInputAttribute;
+                    var input = new Variable(att.Name, field.FieldType, typeOwner, null);
+                    inputs.Add(input);
                 }
             }
+
+            var props = typeOwner.GetProperties();
+            foreach (var prop in props)
+            {
+                var atts = prop.GetCustomAttributes();
+                if (atts.Any(a => a.GetType() == typeof(UtilityInputAttribute)))
+                {
+                    var att = prop.GetCustomAttributes(typeof(UtilityInputAttribute), false)[0] as UtilityInputAttribute;
+                    var input = new Variable(att.Name, prop.PropertyType, typeOwner, null);
+                    inputs.Add(input);
+                }
+            }
+            return inputs.ToArray();
+        }
+
+        public static Variable[] CollectVariables(Type typeOwner,Type variable)
+        {
+            var inputs = CollectVariables(typeOwner).Where(v => v.type == variable);
+            return inputs.ToArray();
+        }
+
+        public static Variable[] CollectVariables(Type typeOwner, Type[] variable)
+        {
+            var vs = CollectVariables(typeOwner);
+            var inputs = vs.Where(v => variable.Contains(v.type));
+            return inputs.ToArray();
+        }
+
+        public static ActionInfo[] CollectActions(Type type)
+        {
+            var actions = new List<ActionInfo>();
+            var meths = type.GetMethods();
+            foreach (var meth in meths)
+            {
+                var atts = meth.GetCustomAttributes();
+                if (atts.Any(a => a.GetType() == typeof(UtilityActionAttribute)))
+                {
+                    var att = meth.GetCustomAttribute(typeof(UtilityActionAttribute), false) as UtilityActionAttribute;
+                    var action = new ActionInfo(att.Name, type, null);
+                    actions.Add(action);
+                }
+            }
+
+            var events = type.GetEvents();
+            foreach (var evt in events)
+            {
+                var atts = evt.GetCustomAttributes();
+                if (atts.Any(a => a.GetType() == typeof(UtilityActionAttribute)))
+                {
+                    var att = evt.GetCustomAttribute(typeof(UtilityActionAttribute), false) as UtilityActionAttribute;
+                    var action = new ActionInfo(att.Name, type, null);
+                    actions.Add(action);
+                }
+            }
+
+            return actions.ToArray();
         }
     }
 
