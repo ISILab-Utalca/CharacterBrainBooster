71143bd79b8905ef3b6b6305b0268470e155ccc1
pokelocos <nare.rare.fire@gmail.com>
2022-12-13 18:01:54 -0300

    Update UitilityPanel

--- CBB-Game/Assets/CBB External Tool/Resources/UtilityPanel.cs
+++ CBB-Game/Assets/CBB External Tool/Resources/UtilityPanel.cs
@@ -1,5 +1,6 @@
 using CBB.Api;
 using CBB.Lib;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,27 +9,68 @@ using UnityEngine.UIElements;
 
 public class UtilityPanel : VisualElement
 {
+    // Top panel
+    private Label agentLabel;
+    private DropdownField otherDropdown;
+    private Button deleteButton;
+
+    // Left panel
     private TextField nameField;
     private VisualElement actionParameters;
 
+    // Middle panel
     private DropdownField evaluatorDropdown;
     private VisualElement evaluatorParamaters;
 
+    // Right panel
     private DropdownField curveDropdown;
     private Toggle invertedToggle;
     private Chart chart;
     private VisualElement curveParameters;
 
     private UtilityEvaluator[] _evaluators;
+    private Type _self;
     private Curve[] _curves;
     private int _cIndex;
+    private Type[] _others;
+    private Type _sOther; 
 
-    public UtilityPanel(AgentData.Consideration consideration)
+    public UtilityPanel(AgentData agent, Consideration consideration, Action OnChange)
     {
         var vt = Resources.Load<VisualTreeAsset>("UtilityPanel");
         vt.CloneTree(this);
         _evaluators = UtilityEvaluator.GetEvaluators().ToArray();
         _curves = Curve.GetCurves().ToArray();
+        _self = agent.type;
+
+        // AgentLabel 
+        this.agentLabel = this.Q<Label>("AgentLabel");
+        this.agentLabel.text = (agent.type.GetCustomAttributes(typeof(UtilityAgentAttribute), false)[0] as UtilityAgentAttribute).Name;
+
+        // OtherDropdown
+        this.otherDropdown = this.Q<DropdownField>("OtherDropdown");
+        _others = UtilitySystem.CollectAgentTypes();
+        var choices = _others.Select(t => (t.GetCustomAttributes(typeof(UtilityAgentAttribute), false)[0] as UtilityAgentAttribute).Name);
+        this.otherDropdown.choices = (choices.Concat(new List<string>() { "Nothing" })).ToList();
+        this.otherDropdown.index = choices.ToList().Count;
+        this.otherDropdown.RegisterCallback<ChangeEvent<string>>(e => {
+            if(this.otherDropdown.index == _others.Length)
+            {
+                _sOther = null;
+            }
+            else
+            {
+                _sOther = _others[otherDropdown.index];
+            }
+        });
+
+
+        // DeleteButton
+        this.deleteButton = this.Q<Button>("DeleteButton");
+        this.deleteButton.clicked += () => { 
+            agent.considerations.Remove(consideration);
+            OnChange?.Invoke();
+        };
 
         // ConsiderationField
         this.nameField = this.Q<TextField>("NameField"); 
@@ -56,14 +98,16 @@ public class UtilityPanel : VisualElement
             UpdateEvaluatorParameter(_evaluators[index]);
         });
 
+        // Curve
+        _cIndex = _curves.ToList().FindIndex(c => c.GetType().Equals(consideration.curve.GetType()));
+
         // CurveParameters
         this.curveParameters = this.Q<VisualElement>("CurveParameters");
+        this.UpdateCurveParamter(_curves[_cIndex]);
 
         // Chart
         this.chart = this.Q<Chart>("Chart");
-        _cIndex = _curves.ToList().FindIndex(c => c.GetType().Equals(consideration.curve.GetType()));
-        Debug.Log(_cIndex);
-        UpdateChart(_curves[_cIndex]);
+        this.UpdateChart(_curves[_cIndex]);
 
         // InvertedToggle
         this.invertedToggle = this.Q<Toggle>("InvertedToggle");
@@ -91,33 +135,62 @@ public class UtilityPanel : VisualElement
 
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="evaluator"></param>
     private void UpdateEvaluatorParameter(UtilityEvaluator evaluator)
     {
+        var allowedTypes = (evaluator.GetType().GetCustomAttributes(typeof(ParamsAllowedAttribute), false)[0] as ParamsAllowedAttribute).Parms;
+        var selfInputs = UtilitySystem.CollectVariables(_self, allowedTypes);
+        var otherInputs = new Variable[0];
+        if (_sOther != null)
+        {
+            otherInputs = UtilitySystem.CollectVariables(_sOther, allowedTypes);
+        }
+        var globalVaribles = Globals.globalVariables.Where(gv => allowedTypes.Contains(gv.type));
+        var choices = new string[0]
+            .Concat(selfInputs.Select(s => s.name))
+            .Concat(otherInputs.Select(o => "(other) " + o.name))
+            .Concat(globalVaribles.Select(g => "(global min) " + g.name))
+            .Concat(globalVaribles.Select(g => "(global max) " + g.name));
+
         this.evaluatorParamaters.Clear();
         var att = evaluator.GetType().GetCustomAttributes(typeof(EvaluatorAttribute), false)[0] as EvaluatorAttribute;
         for (int i = 0; i < att.inputsNames.Length; i++)
         {
             var dropdown = new DropdownField();
             dropdown.label = att.inputsNames[i];
+            dropdown.choices.AddRange(choices);
             evaluatorParamaters.Add(dropdown);
         }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="curve"></param>
     private void UpdateCurveParamter(Curve curve)
     {
         this.curveParameters.Clear();
-        var att = curve.GetType().GetCustomAttributes(typeof(CurveAttribute), false)[0] as CurveAttribute;
-        for (int i = 0; i < att.parms.Length; i++)
+        var p = curve.GetType().GetFields();
+        var parms = p.Where( f => f.GetCustomAttributes(typeof(ParamAttribute), false).Count() > 0).ToList();
+        for (int i = 0; i < parms.Count; i++)
         {
-            var dropdown = new DropdownField();
-            dropdown.label = att.parms[i];
-            dropdown.RegisterCallback<ChangeEvent<string>>(e => {
+            var att = (parms[i].GetCustomAttributes(typeof(ParamAttribute), false)[0] as ParamAttribute);
+            var parm = parms[i];
+            var sliderField = new NumberSliderField(att.Name, (float)parm.GetValue(curve), att.Min, att.Max, (v) => {;
+                parm.SetValue(curve, v);
                 UpdateChart(curve);
-            });
-            curveParameters.Add(dropdown);
+            });;
+            curveParameters.Add(sliderField);
         }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="curve"></param>
     private void UpdateChart(Curve curve)
     {
         chart.SetCurve(curve);
