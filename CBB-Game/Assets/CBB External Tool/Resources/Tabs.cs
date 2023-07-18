b9783fb9a4cbead3e6d700f3cdb5e353b0e703b2
pokelocos <nare.rare.fire@gmail.com>
2023-07-18 10:27:53 -0400

    Add tabs element

--- CBB-Game/Assets/CBB External Tool/Resources/Tabs.cs
+++ CBB-Game/Assets/CBB External Tool/Resources/Tabs.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class Tabs : MonoBehaviour
+namespace CBB.ExternalTool
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Tabs : VisualElement
     {
-        
-    }
+        #region FACTORY
+        public new class UxmlFactory : UxmlFactory<Tabs, VisualElement.UxmlTraits> { }
+        #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // View
+        private ListView List;
+
+        // Info
+        private List<string> target;
+
+        public Tabs()
+        {
+            var visualTree = Resources.Load<VisualTreeAsset>("Tabs");
+            visualTree.CloneTree(this);
+
+            // History list
+            this.List = this.Q<ListView>();
+            List.bindItem += BindItem;
+            List.makeItem += MakeItem;
+            List.itemsChosen += OnItemChosen;
+            List.selectionChanged += OnSelectionChange;
+        }
+
+
+        private VisualElement MakeItem() // hacer que esto sea un solo viewElement (!!!)
+        {
+            var content = new VisualElement();
+            var textLabel = new Label();
+            textLabel.name = "text";
+
+            return content;
+        }
+
+        private void BindItem(VisualElement element, int index)
+        {
+            var nameLabel = element.Q<Label>("text");
+            nameLabel.text = target[index];
+        }
+
+        public void OnSelectionChange(IEnumerable<object> objs)
+        {
+            Debug.Log("OnSelectionChange");
+        }
+
+        public void OnItemChosen(IEnumerable<object> objs)
+        {
+            Debug.Log("OnItemChosen");
+        }
     }
 }
