e242dc1cd3483d121558d559fec4974c1e48609a
dnorambu <diego.norambuena@sansano.usm.cl>
2023-07-25 10:03:35 -0400

    Added sensor list to agent

--- CBB-Game/Assets/ISILab/Scripts/Agent.cs
+++ CBB-Game/Assets/ISILab/Scripts/Agent.cs
@@ -1,6 +1,8 @@
 ﻿using ArtificialIntelligence.Utility;
 using System.Collections.Generic;
 using UnityEngine;
+using Utility;
+
 namespace CBB.Lib
 {
     [System.Serializable]
@@ -10,12 +12,18 @@ namespace CBB.Lib
         /// The data structure that is going to be serialized, then sent to CBB
         /// </summary>
         public AgentBasicData BasicData;
-        
+        public List<SensorBaseClass> AgentSensors = new();
         [ContextMenu("Invoke Start")]
         private void Start()
         {
             BasicData = Generators.New_Agent_Basic_Data();
             Debug.Log($"{this} data: name = {BasicData.agentName}; type = {BasicData.agentType}");
+        }
+        [ContextMenu("Serialize sensor data")]
+        private void SerializeSensorData()
+        {
+            string sensorsData = JSONDataManager.SerializeData(AgentSensors);
+            Debug.Log(sensorsData);
         }
     }
 }
