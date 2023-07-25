4b70a240e7eccd9098e25e3f68b86bc7df554c02
pokelocos <nare.rare.fire@gmail.com>
2023-07-25 16:17:18 -0400

    Update GameData.cs

--- CBB-Game/Assets/CBB External Tool/GameData.cs
+++ CBB-Game/Assets/CBB External Tool/GameData.cs
@@ -1,3 +1,5 @@
+using CBB.Lib;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
@@ -6,18 +8,35 @@ using UnityEngine;
 [System.Serializable]
 public class GameData
 {
+    #region FIELDS
     private TcpClient client;
 
     // Current agent
-    private List<(string, bool)> agents = new List<(string, bool)>();
+    private Dictionary<AgentBasicData, bool> agents = new Dictionary<AgentBasicData, bool>();
 
     // Dictionary<Actor, List<Desicions>>
-    private Dictionary<string, List<string>> histories = new Dictionary<string, List<string>>();
+    private Dictionary<AgentBasicData, List<string>> histories = new Dictionary<AgentBasicData, List<string>>();
     
     // List<Brain>
     private List<string> brains = new List<string>();
+    #endregion
 
-    public List<string> GetHistory(string agent)
+    #region EVENTS
+    public Action<GameData, AgentBasicData> OnAddAgent;
+    public Action<GameData, AgentBasicData> OnAgentSetAsDestroyed;
+    public Action<GameData, AgentBasicData, string> OnAddDecision;
+    public Action<GameData, List<string>> OnAddBrains;
+    #endregion
+
+    #region CONSTRUCTORS
+    public GameData(TcpClient client)
+    {
+        this.client = client;
+    }
+    #endregion
+
+    #region METHODS
+    public List<string> GetHistory(AgentBasicData agent)
     {
         try
         {
@@ -32,15 +51,28 @@ public class GameData
         }
     }
 
-    public void UpdateHistory(string agent, string decision)
+    public void UpdateHistory(AgentBasicData agent, string decision)
     {
         var history = GetHistory(agent);
         history.Add(decision);
+        OnAddDecision?.Invoke(this, agent, decision);
     }
 
-    public GameData(TcpClient client)
+    public void AddAgent(AgentBasicData agent)
     {
-        this.client = client;
+        agents.Add(agent, true);
+        OnAddAgent?.Invoke(this, agent);
     }
 
+    public void RemoveAgent(AgentBasicData agent)
+    {
+        agents.Remove(agent);
+    }
+
+    public void SetAgentAsDestroyed(AgentBasicData agent)
+    {
+        agents[agent] = false;
+        OnAgentSetAsDestroyed?.Invoke(this, agent);
+    }
+    #endregion
 }
