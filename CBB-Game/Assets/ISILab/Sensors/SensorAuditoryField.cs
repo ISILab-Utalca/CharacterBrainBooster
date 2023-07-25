b077f14a8f0a7a0ff93ca825f348e1f8678f899a
dnorambu <diego.norambuena@sansano.usm.cl>
2023-07-25 10:04:04 -0400

    Sensor implementations
    field of view and auditory field

--- CBB-Game/Assets/ISILab/Sensors/SensorAuditoryField.cs
+++ CBB-Game/Assets/ISILab/Sensors/SensorAuditoryField.cs
@@ -1,9 +1,46 @@
 using ArtificialIntelligence.Utility;
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utility;
 
+[JsonObject(MemberSerialization = MemberSerialization.OptIn)]
 public class SensorAuditoryField : SensorBaseClass
 {
+    // Configuration
+    [JsonProperty, SerializeField]
+    private float fieldRadius = 1f;
+    // Individual memory
+    [JsonProperty]
+    public List<GameObject> heardObjects = new();
     
+    // Private references
+    private SphereCollider sphereColl;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        sphereColl = GetComponent<SphereCollider>();
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isDebug) Debug.Log($"Object detected: {other.name}");
+
+        heardObjects.Add(other.gameObject);
+        OnSensorUpdate?.Invoke();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (isDebug) Debug.Log($"Object lost: {other.name}");
+        heardObjects.Remove(other.gameObject);
+        OnSensorUpdate?.Invoke();
+    }
+    [ContextMenu("Serialize sensor")]
+    public void SerializeSensor()
+    {
+        string ss = JSONDataManager.SerializeData(this);
+        Debug.Log(ss);
+    }
 }
--- CBB-Game/Assets/ISILab/Sensors/SensorFieldOfView.cs
+++ CBB-Game/Assets/ISILab/Sensors/SensorFieldOfView.cs
@@ -1,25 +1,61 @@
 using ArtificialIntelligence.Utility;
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utility;
+
 namespace CBB.Lib
 {
+    [RequireComponent(typeof(BoxCollider))]
     public class SensorFieldOfView : SensorBaseClass
     {
+        [SerializeField,SerializeProperty("HorizontalFOV")]
+        private float horizontalFOV = 1;
+        private float verticalFOV = 1;
+        private float rangeOfSight = 1;
         // Individual memory
         public List<GameObject> viewedObjects = new();
+        // Implementation
+        public BoxCollider boxCollider;
+
+        public float HorizontalFOV
+        {
+            get => horizontalFOV;
+            set
+            {
+                horizontalFOV = value;
+                var boxSize = boxCollider.size;
+                if (boxCollider != null)
+                {
+                    boxCollider.size = new Vector3(HorizontalFOV, boxSize.y, boxSize.z);
+                }
+            }
+        }
+        protected override void Awake()
+        {
+            base.Awake();
+            boxCollider = GetComponent<BoxCollider>();
+
+        }
         private void OnTriggerEnter(Collider other)
         {
             if (isDebug) Debug.Log($"Object detected: {other.name}");
 
-            HelperFunctions.AddTargetToList(viewedObjects, other.gameObject);
+            viewedObjects.Add(other.gameObject);
             OnSensorUpdate?.Invoke();
         }
         private void OnTriggerExit(Collider other)
         {
             if (isDebug) Debug.Log($"Object lost: {other.name}");
-            HelperFunctions.RemoveTargetFromList(viewedObjects, other.gameObject);
+            viewedObjects.Remove(other.gameObject);
             OnSensorUpdate?.Invoke();
+        }
+        [ContextMenu("Serialize sensor")]
+        public void SerializeSensor()
+        {
+            string ss = JSONDataManager.SerializeData(this);
+            Debug.Log(ss);
         }
     }
 }
