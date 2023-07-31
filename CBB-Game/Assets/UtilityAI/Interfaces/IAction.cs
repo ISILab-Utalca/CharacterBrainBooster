51b689e495ed35359ea5754a3f946534a6410076
dnorambu <diego.norambuena@sansano.usm.cl>
2023-07-31 15:09:47 -0400

    Actions interface renamed to IAction

--- CBB-Game/Assets/UtilityAI/Interfaces/IAction.cs
+++ CBB-Game/Assets/UtilityAI/Interfaces/IAction.cs
@@ -9,25 +9,25 @@ namespace MixTheForgotten.AI
     /// This interface declares a contract for execute, interrupt and stop actions.
     /// Eventually, this interface could implement async methods, to concatenate actions
     /// </summary>
-    public interface IExecutable
+    public interface IAction
     {
         bool IsRunning { get; set; }
         /// <summary>
         /// Called when the execution is stopped by some other event, before it finishes normally.
         /// Useful for interruptions like a higher priority action that needs to be executed.
         /// </summary>
-        public void InterruptExecution();
+        void InterruptExecution();
         /// <summary>
         /// Call this method when you have reached the end of the normal execution i.e. the action
         /// executed properly, without interuptions.
         /// </summary>
-        public void FinishExecution();
+        void FinishExecution();
         /// <summary>
         /// Begin the execution of this action. Make sure that every action that
         /// overrides this method set the <b>IsRunning</b> property to true
         /// </summary>
         /// <param name="target"></param>
-        public void StartExecution(GameObject target = null);
+        void StartExecution(GameObject target = null);
         /// <summary>
         /// Score an action based on the current context. Every specific action overrides
         /// this method in order to evaluate itself relative to the agent, a target or
@@ -36,7 +36,7 @@ namespace MixTheForgotten.AI
         /// <returns>
         /// The list of available <b>options</b> to execute.
         /// </returns>
-        public List<Option> ScoreOptions();
+        public List<Option> GetOptions();
 
     }
 }
