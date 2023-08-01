8867794f70e579e245c4299e0818170dac9ca1d8
dnorambu <diego.norambuena@sansano.usm.cl>
2023-08-01 12:16:11 -0400

    Creating NPC fields
    Goblin and Villager have their own fields

--- CBB-Game/Assets/ISILab/Agent model/Goblin.cs
+++ CBB-Game/Assets/ISILab/Agent model/Goblin.cs
@@ -1,7 +1,41 @@
-﻿namespace CBB.Lib
+﻿using ArtificialIntelligence.Utility;
+using CBB.Api;
+using UnityEngine;
+namespace CBB.Lib
 {
-    public class Goblin : Agent 
+    public class Goblin : MonoBehaviour, IAgent
     {
+        [UtilityInput("Health")]
+        public float Health = 200f;
+        [UtilityInput("Speed")]
+        public float Speed = 1.0f;
+        [UtilityInput("Attack")]
+        public int Attack = 10;
 
+        public AgentData AgentData;
+        public AgentData GetInternalState()
+        {
+            UpdateInternalState();
+            return AgentData;
+        }
+
+        public void InitializeInternalState()
+        {
+            AgentData = new AgentData
+            {
+                AgentType = typeof(Goblin)
+            };
+            UpdateInternalState();
+        }
+
+        public void UpdateInternalState()
+        {
+            AgentData.fields = UtilitySystem.CollectVariables(typeof(Goblin));
+            var sensors = gameObject.GetComponent<AgentBrain>()._sensors;
+            foreach (var sensor in sensors)
+            {
+                //AgentData.SensorsData.Add(sensor.GetSensorData());
+            }
+        }
     }
 }
--- CBB-Game/Assets/ISILab/Agent model/Villager.cs
+++ CBB-Game/Assets/ISILab/Agent model/Villager.cs
@@ -1,4 +1,40 @@
-﻿namespace CBB.Lib
+﻿using Newtonsoft.Json;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+namespace CBB.Lib
 {
-    public class Villager : Agent { }
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Villager : MonoBehaviour, IAgent
+    {
+        // Tipical stats for an NPC
+        [JsonProperty]
+        public float Health = 100;
+        [JsonProperty]
+        public float Speed = 3;
+
+        // The data member this class has
+        public AgentData agentData;
+
+        public AgentData SetInternalState()
+        {
+            return agentData;
+        }
+        public void InitializeInternalState()
+        {
+            agentData = new AgentData();
+            agentData.AgentType = typeof(Villager);
+            
+        }
+
+        public AgentData GetInternalState()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateInternalState()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
