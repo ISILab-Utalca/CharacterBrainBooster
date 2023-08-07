0adcebe5a7e7a3b2a0d82522e3056a22bc73926a
dnorambu <diego.norambuena@sansano.usm.cl>
2023-08-07 09:58:59 -0400

    property drawer preprocessor added
    Fix errors when building the project

--- CBB-Game/Assets/ISILab/UtilityAI/HelperClasses/TagSelectorPropertyDrawer.cs
+++ CBB-Game/Assets/ISILab/UtilityAI/HelperClasses/TagSelectorPropertyDrawer.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
@@ -74,3 +75,4 @@ public class TagSelectorPropertyDrawer : PropertyDrawer
         }
     }
 }
+#endif
