6eea04800cddb0227911ed30d9e192aa65195d7b
dnorambu <diego.norambuena@sansano.usm.cl>
2023-08-07 09:53:43 -0400

    New property on IAgent interface

--- CBB-Game/Assets/ISILab/Scripts/IAgent.cs
+++ CBB-Game/Assets/ISILab/Scripts/IAgent.cs
@@ -2,6 +2,7 @@ namespace CBB.Lib
 {
     public interface IAgent
     {
+        public AgentData AgentData { get; set; }
         public AgentData GetInternalState();
         public void InitializeInternalState();
         public void UpdateInternalState();
