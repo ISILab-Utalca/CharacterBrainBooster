fa3e3f1e88026a3f3f868314f2bbdfc94030ca01
dnorambu <diego.norambuena@sansano.usm.cl>
2023-09-25 19:04:07 -0300

    Monitoring window controller added
    Handles internal type messages received from server, handles disconnection both from server and user

--- CBB-Game/Assets/CBB External Tool/Controllers/MonitoringWindowController.cs
+++ CBB-Game/Assets/CBB External Tool/Controllers/MonitoringWindowController.cs
@@ -1,20 +1,63 @@
-using CBB.ExternalTool;
-using System.Collections;
-using System.Collections.Generic;
+using CBB.Comunication;
+using System;
 using UnityEngine;
 
-public class MonitoringWindowController : MonoBehaviour
+namespace CBB.ExternalTool
 {
-    [SerializeField]
-    private MonitoringWindow monitoringWindow;
-    [SerializeField]
-    private ExternalMonitor externalMonitor;
-    private void Awake()
+
+    public class MonitoringWindowController : MonoBehaviour
     {
-        monitoringWindow.OnDisconnectedFromServer += externalMonitor.RemoveClient;
-    }
-    private void OnDestroy()
-    {
-        monitoringWindow.OnDisconnectedFromServer -= externalMonitor.RemoveClient;
+        [SerializeField] private ExternalMonitor externalMonitor;
+        private MonitoringWindow monitoringWindow;
+        private void Awake()
+        {
+            monitoringWindow = GetComponent<MonitoringWindow>();
+        }
+        private void OnEnable()
+        {
+            ExternalMonitor.OnMessageReceived += HandleMessage;
+            ExternalMonitor.OnConnectionClosedByServer += HandleServerStop;
+            MonitoringWindow.OnDisconnectionButtonPressed += externalMonitor.HandleUserDisconnection;
+        }
+
+
+        private void OnDisable()
+        {
+            ExternalMonitor.OnMessageReceived -= HandleMessage;
+            ExternalMonitor.OnConnectionClosedByServer -= HandleServerStop;
+            MonitoringWindow.OnDisconnectionButtonPressed -= externalMonitor.HandleUserDisconnection;
+        }
+        private void HandleServerStop()
+        {
+            monitoringWindow.Close();
+            GameData.ClearData();
+        }
+        private void HandleMessage(string msg)
+        {
+            if (Enum.TryParse(typeof(InternalMessage), msg, out object messageType))
+            {
+                switch (messageType)
+                {
+                    case InternalMessage internalMessage:
+                        InternalCallback(internalMessage);
+                        return;
+                    default:
+                        break;
+                }
+            }
+        }
+        private void InternalCallback(InternalMessage message)
+        {
+            switch (message)
+            {
+                case InternalMessage.SERVER_STOPPED:
+                    //externalMonitor.RemoveClient();
+                    monitoringWindow.Close();
+                    break;
+                default:
+                    Debug.LogWarning($"Message: {message} | Is not being implemented yet");
+                    break;
+            }
+        }
     }
 }
