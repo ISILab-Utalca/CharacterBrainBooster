92264d1d11f37491c4ee8e271ea2403e6c3a2ded
dnorambu <diego.norambuena@sansano.usm.cl>
2023-09-25 19:03:19 -0300

    Main window controller added

--- CBB-Game/Assets/CBB External Tool/Controllers/MainWindowController.cs
+++ CBB-Game/Assets/CBB External Tool/Controllers/MainWindowController.cs
@@ -1,21 +1,48 @@
+using CBB.Comunication;
 using CBB.ExternalTool;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainWindowController : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField] private ExternalMonitor externalMonitor;
+    [SerializeField] private MonitoringWindow monitoringWindow;
+    [SerializeField] private EditorWindow editorWindow;
     private MainWindow mainWindow;
-    [SerializeField]
-    private ExternalMonitor externalMonitor;
 
     private void Awake()
     {
+        mainWindow = GetComponent<MainWindow>();
         mainWindow.OnConnectionToServerStarted += externalMonitor.ConnectToServer;
+        MonitoringWindow.OnDisconnectionButtonPressed += OpenWindow;
+        ExternalMonitor.OnConnectionClosedByServer += OpenWindow;
+        ExternalMonitor.OnServerConnected += CloseWindow;
     }
+
     private void OnDestroy()
     {
         mainWindow.OnConnectionToServerStarted -= externalMonitor.ConnectToServer;
+        MonitoringWindow.OnDisconnectionButtonPressed -= OpenWindow;
+        ExternalMonitor.OnConnectionClosedByServer -= OpenWindow;
+        ExternalMonitor.OnServerConnected -= CloseWindow;
+    }
+    private void OpenWindow()
+    {
+        gameObject.SetActive(true);
+    }
+    private void CloseWindow()
+    {
+        switch (mainWindow.startMode.value) 
+        {
+            case 0:
+                monitoringWindow.gameObject.SetActive(true); break;
+            case 1:
+                editorWindow.gameObject.SetActive(true); break;
+            default
+                : break;
+        }
+        gameObject.SetActive(false);
     }
 }
