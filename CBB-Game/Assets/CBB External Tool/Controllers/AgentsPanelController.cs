c855a2baeb1ee60b33663e32ecd800bffa4e5c09
dnorambu <diego.norambuena@sansano.usm.cl>
2023-09-25 19:02:52 -0300

    Agent panel controller added

--- CBB-Game/Assets/CBB External Tool/Controllers/AgentsPanelController.cs
+++ CBB-Game/Assets/CBB External Tool/Controllers/AgentsPanelController.cs
@@ -1,26 +1,63 @@
-using CBB.ExternalTool;
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using CBB.Api;
+using Newtonsoft.Json;
 using UnityEngine;
-
-public class AgentsPanelController : MonoBehaviour
+namespace CBB.ExternalTool
 {
-    [SerializeField]
-    private MonitoringWindow monitoringWindow;
 
-    private AgentsPanel agentsPanel;
-    private void OnEnable()
+    public class AgentsPanelController : MonoBehaviour, IMessageHandler
     {
-        monitoringWindow.OnSetupComplete += Initialize;
-    }
-    private void OnDisable()
-    {
-        monitoringWindow.OnSetupComplete -= Initialize;
-    }
-    private void Initialize()
-    {
-        agentsPanel = monitoringWindow.AgentsPanel;
+        [SerializeField]
+        private MonitoringWindow monitoringWindow;
 
+        private AgentsPanel agentsPanel;
+        private HistoryPanel historyPanel;
+        
+        readonly JsonSerializerSettings settings = new()
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            MissingMemberHandling = MissingMemberHandling.Error
+        };
+
+        private void Awake()
+        {
+            monitoringWindow.OnSetupComplete += Initialize;
+        }
+        private void OnEnable()
+        {
+            ExternalMonitor.OnMessageReceived += HandleMessage;
+        }
+
+        private void OnDisable()
+        {
+            ExternalMonitor.OnMessageReceived -= HandleMessage;
+        }
+        private void OnDestroy()
+        {
+            monitoringWindow.OnSetupComplete -= Initialize;
+        }
+        private void Initialize()
+        {
+            agentsPanel = monitoringWindow.AgentsPanel;
+            historyPanel = monitoringWindow.HistoryPanel;
+
+        }
+        public void HandleMessage(string message)
+        {
+            try
+            {
+                var agentWrapper = JsonConvert.DeserializeObject<AgentWrapper>(message, settings);
+                // Pass to the model side of the app
+                GameData.HandleAgentWrapper(agentWrapper);
+                // Update the UI
+                agentsPanel.Refresh();
+                return;
+            }
+            catch (System.Exception ex)
+            {
+                Debug.Log("<color=red>[AGENTS PANEL CONTROLLER] Message is not an AgentWrapper: </color>" + ex);
+            }
+        }
+        
     }
+
 }
