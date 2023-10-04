54483ba6c00791d156ba1b8a9b8ff14f2a5d91a7
dnorambu <diego.norambuena@sansano.usm.cl>
2023-10-04 19:10:29 -0300

    Reorganization of scripts
    The custom controls now live in a dedicated folder (no longer in Resources)

---
+++ CBB-Game/Assets/CBB External Tool/Custom UI Controls/ConsiderationView.cs
@@ -0,0 +1,37 @@
+using CBB.Lib;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace CBB.ExternalTool
+{
+    public class ConsiderationView : VisualElement
+    {
+        #region FACTORY
+        public new class UxmlFactory : UxmlFactory<ConsiderationView, VisualElement.UxmlTraits> { }
+        #endregion
+
+        // View
+        private Chart chart;
+        private Foldout foldout;
+        private Label input;
+        private Label utility;
+        public ConsiderationView()
+        {
+            var visualTree = Resources.Load<VisualTreeAsset>("ConsiderationView");
+            visualTree.CloneTree(this);
+
+            // Chart
+            chart = this.Q<Chart>();
+            foldout = this.Q<Foldout>();
+            input = this.Q<Label>("input");
+            utility = this.Q<Label>("utility");
+        }
+        public void ShowConsideration(ConsiderationData consideration)
+        {
+            chart.SetCurve(consideration.Curve, consideration.InputValue);
+            foldout.text = consideration.ConsiderationName;
+            input.text = consideration.EvaluatedVariableName + consideration.InputValue.ToString();
+            utility.text = "Utility: " + consideration.UtilityValue.ToString();
+        }
+    }
+}
---
+++ CBB-Game/Assets/CBB External Tool/Custom UI Controls/GraphicsPanel.cs
@@ -0,0 +1,55 @@
+using CBB.ExternalTool;
+using CBB.Lib;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GraphicsPanel : VisualElement
+{
+    #region FACTORY
+    public new class UxmlFactory : UxmlFactory<GraphicsPanel, UxmlTraits> { }
+    #endregion
+    #region PROPERTIES
+    public Label ActionName { get; set; }
+    public Label TotalUtility { get;set; }
+    public Chart Chart { get; set; }
+    
+    private ListView evaluationsList;
+    private List<ConsiderationData> aux_considerations;
+    #endregion
+    public GraphicsPanel()
+    {
+        var visualTree = Resources.Load<VisualTreeAsset>("GraphicsPanel");
+        visualTree.CloneTree(this);
+
+        this.ActionName = this.Q<Label>("action-name");
+        this.Chart = this.Q<Chart>();
+        this.evaluationsList = this.Q<ListView>("evaluations-list");
+        this.TotalUtility = this.Q<Label>("total-utility-score");
+
+        evaluationsList.makeItem = MakeItem;
+        evaluationsList.bindItem = BindItem;
+        evaluationsList.itemsSource = aux_considerations;
+    }
+
+    private void BindItem(VisualElement element, int index)
+    {
+        if(element is ConsiderationView cv)
+        {
+            cv.ShowConsideration(aux_considerations[index]);
+        }
+    }
+
+    private VisualElement MakeItem()
+    {
+        return new ConsiderationView();
+    }
+
+    public void DisplayEvaluatedConsiderations(List<ConsiderationData> evaluatedConsiderations)
+    {
+        aux_considerations = evaluatedConsiderations;
+        evaluationsList.itemsSource = aux_considerations;
+        evaluationsList.RefreshItems();
+    }
+}
