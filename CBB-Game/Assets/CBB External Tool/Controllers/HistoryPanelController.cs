6f7323e76a7f4f894d16a50f27bd7bdb2e076ea3
noram <diego.norambuena@sansano.usm.cl>
2023-11-27 11:02:10 -0300

    Remove unnecesary using statements

--- CBB-Game/Assets/CBB External Tool/Controllers/HistoryPanelController.cs
+++ CBB-Game/Assets/CBB External Tool/Controllers/HistoryPanelController.cs
@@ -6,9 +6,7 @@ using System.Collections.ObjectModel;
 using UnityEngine;
 using UnityEngine.UIElements;
 using System.Text.RegularExpressions;
-using UnityEditor;
 using System.Linq;
-using System.Reflection;
 
 namespace CBB.ExternalTool
 {
