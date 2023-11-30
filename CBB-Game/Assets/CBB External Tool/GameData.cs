39bcfb50941244bcbd65584c08c22425b2a7ac7f
noram <diego.norambuena@sansano.usm.cl>
2023-11-29 23:21:09 -0300

    Game Data update
    Limit max list size (change made by Nico) using variable; store decisions and sensor events on custom data structure for a cleaner/simple access to the agent history

--- CBB-Game/Assets/CBB External Tool/GameData.cs
+++ CBB-Game/Assets/CBB External Tool/GameData.cs
@@ -4,14 +4,22 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using UnityEngine;
 
 [Serializable]
 public static class GameData
 {
+    internal class DecisionsAndSensorEvents
+    {
+        internal List<DecisionPackage> Decisions { get; set; } = new List<DecisionPackage>();
+        internal List<SensorPackage> SensorEvents { get; set; } = new List<SensorPackage>();
+    }
+
     #region PROPERTIES
     public static Dictionary<int, AgentData> AgentStats { get; set; } = new();
     public static ObservableCollection<(int, string)> Agent_ID_Name { get; set; } = new();
-    public static Dictionary<int, ObservableCollection<AgentPackage>> Histories { get; set; } = new();
+    private static Dictionary<int, DecisionsAndSensorEvents> Histories { get; set; } = new();
+    private static int MaxItems { get; set; } = 200;
     #endregion
     #region EVENTS
     public static Action<AgentData> OnAddAgent { get; set; }
@@ -21,23 +29,49 @@ public static class GameData
     #endregion
 
     #region METHODS
-    public static void Add()
+    /// <summary>
+    /// Get all the decisions taken by this agent
+    /// </summary>
+    /// <param name="agentId">Identifier of the agent</param>
+    /// <returns>A list of <see cref="DecisionPackage"/> in chronological order</returns>
+    public static List<DecisionPackage> GetAgentDecisions(int agentId)
     {
-
+        var decisions = Histories[agentId].Decisions;
+        if (decisions == null)
+        {
+            Debug.LogError($"Decisions list of agent id {agentId} is empty");
+        }
+        return decisions;
     }
-
-    public static ObservableCollection<AgentPackage> GetHistory(int agentID)
+    /// <summary>
+    /// Get all the sensor events for this particular agent
+    /// </summary>
+    /// <param name="agentId">Identifier of the agent</param>
+    /// <returns>A list of <see cref="SensorPackage"/> in chronological order</returns>
+    public static List<SensorPackage> GetAgentSensorEvents(int agentId)
     {
-        try
+        var sensorEvents = Histories[agentId].SensorEvents;
+        if (sensorEvents == null)
         {
-            return Histories[agentID];
+            Debug.LogError($"Sensor events list of agent id {agentId} is empty");
         }
-        catch
-        {
-            Histories.Add(agentID, new ObservableCollection<AgentPackage>());
-            var history = Histories[agentID];
-            return history;
-        }
+        return sensorEvents;
+    }
+    /// <summary>
+    /// Get all the decisions and sensor events of this agent
+    /// </summary>
+    /// <param name="agentID">Identifier of the agent</param>
+    /// <returns>A list of <see cref="AgentPackage"/> (decisions and sensor events) sorted by timestamp</returns>
+    public static List<AgentPackage> GetAgentFullHistory(int agentID)
+    {
+        var allDecisions = Histories[agentID].Decisions;
+        var allSensorEvents = Histories[agentID].SensorEvents;
+        var fullHistory = new List<AgentPackage>();
+        fullHistory.AddRange(allDecisions);
+        fullHistory.AddRange(allSensorEvents);
+        // Sort by timestamp
+        fullHistory = fullHistory.OrderBy(o => o.timestamp).ToList();
+        return fullHistory;
     }
 
     public static void HandleAgentWrapper(AgentWrapper agent)
@@ -59,12 +93,12 @@ public static class GameData
     }
     private static void AddAgentState(AgentData agent)
     {
-        if (AgentStats.ContainsKey(agent.ID)) 
+        if (AgentStats.ContainsKey(agent.ID))
             return;
 
         AgentStats.Add(agent.ID, agent);
 
-        if (Agent_ID_Name.Contains((agent.ID, agent.agentName))) 
+        if (Agent_ID_Name.Contains((agent.ID, agent.agentName)))
             return;
 
         Agent_ID_Name.Add((agent.ID, agent.agentName));
@@ -90,28 +124,46 @@ public static class GameData
         AgentStats[agent.ID] = null;
         OnAgentSetAsDestroyed?.Invoke(agent);
     }
-
-    internal static void HandleDecisionPackage(AgentPackage package)
+    /// <summary>
+    /// If the agent is registered, add the sensor event to its history. Else,
+    /// register the agent and add the new sensor event
+    /// </summary>
+    /// <param name="package"></param>
+    public static void HandleSensorEventPackage(SensorPackage package)
     {
-        if (Histories.ContainsKey(package.agentID))
+
+        if (Histories.TryGetValue(package.agentID, out var data))
         {
-            var h = Histories[package.agentID];
-            if(h.Count <= 200)
-            {
-                h.Add(package);
-            }
-            else
-            {
-                h.RemoveAt(h.Count - 1);
-                h.Add(package);
-            }
+            if (data.SensorEvents.Count >= MaxItems) data.SensorEvents.RemoveAt(0);
+            data.SensorEvents.Add(package);
         }
         else
         {
-            Histories.Add(package.agentID, new ObservableCollection<AgentPackage>() { package });
+            RegisterNewAgentHistory(package.agentID);
+            Histories[package.agentID].SensorEvents.Add(package);
         }
-        OnAddDecision?.Invoke(package);
     }
+
+    public static void HandleDecisionPackage(DecisionPackage package)
+    {
+
+        if (Histories.TryGetValue(package.agentID, out var data))
+        {
+            if (data.Decisions.Count >= MaxItems) data.Decisions.RemoveAt(0);
+            data.Decisions.Add(package);
+        }
+        else
+        {
+            RegisterNewAgentHistory(package.agentID);
+            Histories[package.agentID].Decisions.Add(package);
+        }
+
+    }
+    private static void RegisterNewAgentHistory(int agentID)
+    {
+        Histories.Add(agentID, new DecisionsAndSensorEvents());
+    }
+
 
     internal static void ClearData()
     {
