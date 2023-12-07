256e985656250332b824a671ed7c1cc801933ff3
dnorambu <diego.norambuena@sansano.usm.cl>
2023-12-07 09:35:00 -0300

    Notify when a new client connects to the server

--- CBB-Game/Assets/Comunication/NewClientNotifier.cs
+++ CBB-Game/Assets/Comunication/NewClientNotifier.cs
@@ -1,18 +1,25 @@
-using System;
-using System.Net;
+using System.Collections;
 using System.Net.Sockets;
 using UnityEngine;
 namespace CBB.Comunication
 {
     public class NewClientNotifier : MonoBehaviour
     {
-        private void Update()
-        {
-            if(Server.GetNewClientConnected(out TcpClient client)) 
-            {
-                Server.NotifyNewClienConnection(client);
-            }
+        [SerializeField, Tooltip("How often this scripts checks for new connected clients")]
+        private float checkTime = 1;
 
+        private WaitForSeconds wfs = new(1);
+        private IEnumerator Start()
+        {
+            wfs = new WaitForSeconds(checkTime);
+            while (true)
+            {
+                if (Server.GetNewClientConnected(out TcpClient client))
+                {
+                    Server.NotifyNewClienConnection(client);
+                }
+                yield return wfs;
+            }
         }
     }
 
