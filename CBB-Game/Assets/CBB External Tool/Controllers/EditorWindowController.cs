033db7f63222977c3982740132cc50b0a8c3176e
dnorambu <diego.norambuena@sansano.usm.cl>
2023-12-07 16:18:21 -0300

    Reload button added to Editor Window
    Enables loading brain information from memory. This data is received when the client connects to the server, but is not available when entering to Edit Mode directly (latency issues maybe), so this button fix that behavior

--- CBB-Game/Assets/CBB External Tool/Controllers/EditorWindowController.cs
+++ CBB-Game/Assets/CBB External Tool/Controllers/EditorWindowController.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -9,7 +8,7 @@ namespace CBB.ExternalTool
     {
         // UI
         private TreeView brainTree;
-
+        private Button reloadBrainsButton;
         // Logic
         [SerializeField]
         private BrainDataManager brainDataManager;
@@ -18,38 +17,36 @@ namespace CBB.ExternalTool
         {
             var root = GetComponent<UIDocument>().rootVisualElement;
             brainTree = root.Q<TreeView>("brain-tree");
+            reloadBrainsButton = root.Q<Button>("reload-brains-button");
+            reloadBrainsButton.clicked += LoadBrains;
+            // Set TreeView.makeItem to initialize each node in the tree.
+            brainTree.makeItem = MakeItem;
+
+            // Set TreeView.bindItem to bind an initialized node to a data item.
+            brainTree.bindItem = BindItem;
         }
         /// <summary>
         /// Retrieve all the brains existing in the game
         /// </summary>
         public void LoadBrains()
         {
-            var brains = brainDataManager.GetBrains();
             // Display the brains in the tree view
-            brainTree.Clear();
-            // Expresses planet data as a list of TreeViewItemData objects. Needed for TreeView and MultiColumnTreeView.
-            //protected static IList<TreeViewItemData<IPlanetOrGroup>> treeRoots
-            //    {
-            //        get
-            //        {
-            //            int id = 0;
-            //            var roots = new List<TreeViewItemData<IPlanetOrGroup>>(planetGroups.Count);
-            //            foreach (var group in planetGroups)
-            //            {
-            //                var planetsInGroup = new List<TreeViewItemData<IPlanetOrGroup>>(group.planets.Count);
-            //                foreach (var planet in group.planets)
-            //                {
-            //                    planetsInGroup.Add(new TreeViewItemData<IPlanetOrGroup>(id++, planet));
-            //                }
-
-            //                roots.Add(new TreeViewItemData<IPlanetOrGroup>(id++, group, planetsInGroup));
-            //            }
-            //            return roots;
-            //        }
-            //    }
+            //brainTree.Clear();
+            // Call brainTree.SetRootItems() to populate the data in the tree.
+            brainTree.SetRootItems(brainDataManager.TreeRoots);
+            brainTree.RefreshItems();
         }
         private void OnEnable()
+        { 
+            LoadBrains();
+        }
+        private VisualElement MakeItem()
         {
+            return new Label();
+        }
+        private void BindItem(VisualElement element, int index)
+        {
+            (element as Label).text = brainTree.GetItemDataForIndex<IDataItem>(index).GetItemName();
         }
     }
 }
