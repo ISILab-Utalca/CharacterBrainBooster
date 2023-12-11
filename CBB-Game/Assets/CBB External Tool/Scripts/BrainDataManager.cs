083e3cffb3d6c0ba845f6f3f6fc713e9996026ae
dnorambu <diego.norambuena@sansano.usm.cl>
2023-12-11 17:06:34 -0300

    Brain Data Manager correctly displays brains info

--- CBB-Game/Assets/CBB External Tool/Scripts/BrainDataManager.cs
+++ CBB-Game/Assets/CBB External Tool/Scripts/BrainDataManager.cs
@@ -38,23 +38,25 @@ namespace CBB.ExternalTool
                     // Second level: actions and sensors
                     var actionWrapper = new ItemWrapper("Actions");
                     var actionItems = new List<TreeViewItemData<IDataItem>>();
-                    //foreach (var action in brain.serializedActions)
-                    //{
-                    //    actionItems.Add(new TreeViewItemData<IDataItem>(id++, action));
-                    //}
-                    //brainDataGroup.Add(new TreeViewItemData<IDataItem>(id++, actionWrapper, actionItems));
-                    
-                    var sensorWrapper = new ItemWrapper("Sensors");
-                    //var sensorItems = new List<TreeViewItemData<IDataItem>>(brain.serializedSensors.Count);
-                    //foreach (var sensor in brain.serializedSensors)
-                    //{
-                    //    sensorItems.Add(new TreeViewItemData<IDataItem>(id++, sensor));
-                    //}
-                    //brainDataGroup.Add(new TreeViewItemData<IDataItem>(id++, sensorWrapper, sensorItems));
+                    foreach (var action in brain.serializedActions)
+                    {
+                        actionItems.Add(new TreeViewItemData<IDataItem>(id++, action));
+                    }
 
-                    // Wrap up all the items in the tree view
-                    brainDataGroup.Add(new TreeViewItemData<IDataItem>(id++, actionWrapper));
-                    brainDataGroup.Add(new TreeViewItemData<IDataItem>(id++, sensorWrapper));
+                    var sensorWrapper = new ItemWrapper("Sensors");
+                    var sensorItems = new List<TreeViewItemData<IDataItem>>(brain.serializedSensors.Count);
+                    foreach (var sensor in brain.serializedSensors)
+                    {
+                        sensorItems.Add(new TreeViewItemData<IDataItem>(id++, sensor));
+                    }
+
+                    // Wrap up all the items in the tree view (test version, only parent label)
+                    //brainDataGroup.Add(new TreeViewItemData<IDataItem>(id++, actionWrapper));
+                    //brainDataGroup.Add(new TreeViewItemData<IDataItem>(id++, sensorWrapper));
+
+                    // Production version, with children
+                    brainDataGroup.Add(new TreeViewItemData<IDataItem>(id++, actionWrapper, actionItems));
+                    brainDataGroup.Add(new TreeViewItemData<IDataItem>(id++, sensorWrapper, sensorItems));
                     roots.Add(new TreeViewItemData<IDataItem>(id++, brain, brainDataGroup));
                 }
                 return roots;
