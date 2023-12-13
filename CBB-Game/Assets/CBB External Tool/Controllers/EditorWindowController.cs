7cc8d8447437a027e211c9ebd7d6a086e8ac24c1
dnorambu <diego.norambuena@sansano.usm.cl>
2023-12-13 17:53:44 -0300

    Display details of Tree View elements

--- CBB-Game/Assets/CBB External Tool/Controllers/EditorWindowController.cs
+++ CBB-Game/Assets/CBB External Tool/Controllers/EditorWindowController.cs
@@ -1,4 +1,8 @@
+using CBB.Lib;
+using Generic;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -12,35 +16,116 @@ namespace CBB.ExternalTool
         // UI
         private TreeView brainTree;
         private Button reloadBrainsButton;
+        private VisualElement detailsPanel;
+
         // Logic
         [SerializeField]
         private BrainDataManager brainDataManager;
-
+        private ConsiderationEditorController considerationEditorController;
+        [SerializeField]
+        private bool showLogs = false;
         private void Awake()
         {
+            considerationEditorController = GetComponent<ConsiderationEditorController>();
             var root = GetComponent<UIDocument>().rootVisualElement;
             brainTree = root.Q<TreeView>("brain-tree");
             reloadBrainsButton = root.Q<Button>("reload-brains-button");
+            detailsPanel = root.Q<VisualElement>("details-panel");
             reloadBrainsButton.clicked += LoadBrains;
-            // Set TreeView.makeItem to initialize each node in the tree.
             brainTree.makeItem = MakeItem;
-
-            // Set TreeView.bindItem to bind an initialized node to a data item.
             brainTree.bindItem = BindItem;
+            brainTree.selectedIndicesChanged += OnElementSelected;
         }
+
+        private void OnElementSelected(IEnumerable<int> enumerable)
+        {
+            Debug.Log($"[Editor Window Controller] Selected index: {enumerable.First()}");
+            Debug.Log($"[Editor Window Controller] Selected item: {brainTree.selectedItem}");
+            if (brainTree.selectedItem is DataGeneric data)
+            {
+                if (showLogs) Debug.Log($"[Editor Window Controller] Selected brain: {data}");
+                DisplayDataGenericDetails(data);
+            }
+            else if (brainTree.selectedItem is ConsiderationConfiguration config)
+            {
+                if (showLogs) Debug.Log($"[Editor Window Controller] Selected config: {config}");
+                DisplayConsiderationEditor(config);
+            }
+        }
+
+        private void DisplayConsiderationEditor(ConsiderationConfiguration config)
+        {
+            detailsPanel.Clear();
+            var ce = new ConsiderationEditor();
+            considerationEditorController.SetConsiderationEditor(ce);
+            considerationEditorController.ShowConsideration(config);
+            detailsPanel.Add(ce);
+        }
+
+        private void DisplayDataGenericDetails(DataGeneric data)
+        {
+            detailsPanel.Clear();
+            foreach (var item in data.Values)
+            {
+                // Create the visual elements for the data, asking by the type (subclass of WrapperValue)
+                switch (item)
+                {
+                    case WraperString wraper:
+                        var textField = new TextField
+                        {
+                            label = wraper.name,
+                            value = wraper.value
+                        };
+                        textField.style.color = Color.white;
+                        detailsPanel.Add(textField);
+                        break;
+                    case WraperNumber wraper:
+                        var floatField = new FloatField
+                        {
+                            label = wraper.name,
+                            value = wraper.value
+                        };
+                        floatField.style.color = Color.white;
+
+                        detailsPanel.Add(floatField);
+                        break;
+                    case WraperBoolean wraper:
+                        var toggle = new Toggle
+                        {
+                            label = wraper.name,
+                            value = wraper.value
+                        };
+                        toggle.style.color = Color.white;
+
+                        detailsPanel.Add(toggle);
+                        break;
+                    case WrapperConsideration wraper:
+                        var gc = new GenericCard();
+                        gc.SetTitle(wraper.name);
+                        gc.SetSubtitle("Consideration");
+                        detailsPanel.Add(gc);
+                        //var considerationEditor = new ConsiderationEditor();
+                        //considerationEditorController.SetConsiderationEditor(considerationEditor);
+                        //considerationEditorController.ShowConsideration(wraper.configuration);
+                        //detailsPanel.Add(considerationEditor);
+                        break;
+                    default:
+                        Debug.Log($"[Editor Window Controller] WraperValue: {item}");
+                        break;
+                }
+            }
+        }
+
         /// <summary>
         /// Retrieve all the brains existing in the game
         /// </summary>
         public void LoadBrains()
         {
-            // Display the brains in the tree view
-            //brainTree.Clear();
-            // Call brainTree.SetRootItems() to populate the data in the tree.
             brainTree.SetRootItems(brainDataManager.TreeRoots);
             brainTree.RefreshItems();
         }
         private void OnEnable()
-        { 
+        {
             LoadBrains();
         }
         private VisualElement MakeItem()
