98429061d3a95a002abf2028aafdbbb98cf102a7
dnorambu <diego.norambuena@sansano.usm.cl>
2024-01-09 12:04:15 -0300

    [UI External] Editor Window Controller update
    Moved a lot of functionality to other classes, only handles specific window related tools (close, find TcpClient, send brain to game)

--- CBB-Game/Assets/CBB External Tool/Controllers/EditorWindowController.cs
+++ CBB-Game/Assets/CBB External Tool/Controllers/EditorWindowController.cs
@@ -1,14 +1,12 @@
+using ArtificialIntelligence.Utility;
 using CBB.Lib;
+using CBB.UI;
 using Generic;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEngine.Windows;
-
 
 namespace CBB.ExternalTool
 {
@@ -16,20 +14,15 @@ namespace CBB.ExternalTool
     {
         #region FIELDS
         // UI
-        private TreeView brainTree;
-        private Button reloadBrainsButton;
-        private VisualElement detailsPanel;
-        private Button saveBrainButton;
-        private Button closeButton;
         [SerializeField]
         private GameObject mainMenu;
+        private BrainEditor brainEditor;
+        private Button closeButton;
         // Logic
-        [SerializeField]
-        private BrainDataManager brainDataManager;
-        [SerializeField]
+
+        [SerializeField,SerializeProperty("ShowLogs")]
         private bool showLogs = false;
-        private Brain lastSelectedBrain;
-        private ConsiderationEditorController considerationEditorController;
+        private ExternalMonitor monitor;
         private readonly JsonSerializerSettings settings = new()
         {
             ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
@@ -39,209 +32,60 @@ namespace CBB.ExternalTool
             MissingMemberHandling = MissingMemberHandling.Ignore,
             Formatting = Formatting.Indented
         };
-        private ExternalMonitor monitor;
         #endregion
+        public bool ShowLogs
+        {
+            get
+            {
+                return showLogs;
+            }
+            set
+            {
+                showLogs = value;
+                if(brainEditor != null)
+                {
+                    brainEditor.ShowLogs = value;
+                }
+            }
+        }
 
         private void Awake()
         {
-            // Find UI references
-            considerationEditorController = GetComponent<ConsiderationEditorController>();
             var root = GetComponent<UIDocument>().rootVisualElement;
-            brainTree = root.Q<TreeView>("brain-tree");
-            reloadBrainsButton = root.Q<Button>("reload-brains-button");
-            saveBrainButton = root.Q<Button>("save-brain-button");
-            detailsPanel = root.Q<VisualElement>("details-panel");
+            
+            brainEditor = root.Q<BrainEditor>();
+
             closeButton = root.Q<TopTitleBar>().CloseButton;
-
-            // Add listeners to UI elements
-            reloadBrainsButton.clicked += DisplayBrainsTreeView;
-            saveBrainButton.clicked += SaveBrain;
-            brainTree.makeItem = MakeItem;
-            brainTree.bindItem = BindItem;
-            brainTree.selectedIndicesChanged += OnElementSelected;
-            closeButton.clicked += BackToMainMenu;
-
+            
+            // Set reference to TCP Client in order to exchange messages with the server
             monitor = GameObject.Find("External Monitor").GetComponent<ExternalMonitor>();
-        }
 
+            closeButton.clicked += BackToMainMenu;
+            
+            BrainDataManager.ReceivedBrains += brainEditor.SetBrains;
+
+            brainEditor.SetReloadButtonCallback(SendBrain);
+            //TODO: set this through message and not directly from the class
+            brainEditor.EvaluationMethods = ConsiderationMethods.GetAllMethodNames();
+        }
         private void BackToMainMenu()
         {
             mainMenu.SetActive(true);
             gameObject.SetActive(false);
         }
-
-        private void OnEnable()
+        private void SendBrain()
         {
-            DisplayBrainsTreeView();
-        }
-
-        private void SaveBrain()
-        {
-            if(lastSelectedBrain == null)
+            var b = brainEditor.LastSelectedBrain;
+            if(b == null)
             {
                 Debug.LogWarning("[Editor Window Controller] No brain selected");
                 return;
             }
-            var b = lastSelectedBrain;
-            // Save the updated file to a local path
-            //string path = "C:\\Users\\diego\\Escritorio\\Docs" + b.brain_ID + ".json";
-            //System.IO.File.WriteAllText(path, json);
-            //Debug.Log($"[Editor Window Controller] Brain saved to: {path}");
             string json = JsonConvert.SerializeObject(b, settings);
+            
             //Send to server
             monitor.SendData(json);
         }
-        private void OnElementSelected(IEnumerable<int> enumerable)
-        {
-            var index = enumerable.First();
-            var item = brainTree.selectedItem;
-
-            //Cache the last selected brain for saving purposes.
-            //You need to call GetParentBrain since the user can select any item in the tree
-            lastSelectedBrain = item is Brain b ? b : GetParentBrain(index);
-            
-            // Reset the selection on the UI, so the user doesn't get confused
-            brainTree.SetSelectionByIdWithoutNotify(new List<int>() { brainTree.GetIdForIndex(index) });
-            if (item is DataGeneric data)
-            {
-                if (showLogs) Debug.Log($"[Editor Window Controller] Selected brain: {data}");
-                DisplayDataGenericDetails(data);
-            }
-            else if (item is ConsiderationConfiguration config)
-            {
-                if (showLogs) Debug.Log($"[Editor Window Controller] Selected config: {config}");
-                DisplayConsiderationEditor(config);
-            }
-        }
-        private void DisplayConsiderationEditor(ConsiderationConfiguration config)
-        {
-            detailsPanel.Clear();
-            var ce = considerationEditorController.CreateEditor(config);
-            detailsPanel.Add(ce);
-        }
-        private void DisplayDataGenericDetails(DataGeneric data)
-        {
-            detailsPanel.Clear();
-            foreach (var item in data.Values)
-            {
-                // Create the visual elements for the data by its type
-                switch (item)
-                {
-                    case WraperString wraper:
-                        var textField = new TextField
-                        {
-                            label = wraper.name,
-                            value = wraper.value
-                        };
-                        // Bind the value of the text field to the value of the wrapper
-                        textField.RegisterValueChangedCallback((evt) => wraper.value = evt.newValue);
-                        textField.style.color = Color.white;
-                        detailsPanel.Add(textField);
-                        break;
-                    case WraperNumber wraper:
-                        var floatField = new FloatField
-                        {
-                            label = wraper.name,
-                            value = wraper.value
-                        };
-                        floatField.RegisterValueChangedCallback((evt) => wraper.value = evt.newValue);
-                        floatField.style.color = Color.white;
-
-                        detailsPanel.Add(floatField);
-                        break;
-                    case WraperBoolean wraper:
-                        var toggle = new Toggle
-                        {
-                            label = wraper.name,
-                            value = wraper.value
-                        };
-                        toggle.style.color = Color.white;
-                        toggle.RegisterValueChangedCallback((evt) => wraper.value = evt.newValue);
-
-                        detailsPanel.Add(toggle);
-                        break;
-                    case WrapperConsideration wraper:
-                        var gc = new GenericCard();
-                        gc.SetTitle(wraper.name);
-                        gc.SetSubtitle("Consideration");
-                        detailsPanel.Add(gc);
-                        break;
-                    default:
-                        Debug.Log($"[Editor Window Controller] Unidentified: {item}");
-                        break;
-                }
-            }
-        }
-        public void DisplayBrainsTreeView()
-        {
-            brainTree.SetRootItems(brainDataManager.TreeRoots);
-            brainTree.RefreshItems();
-        }
-        private VisualElement MakeItem()
-        {
-            return new Label();
-        }
-        private void BindItem(VisualElement element, int index)
-        {
-            var item = element as Label;
-            var itemName = brainTree.GetItemDataForIndex<IDataItem>(index).GetItemName();
-            // Remove the namespace from the item name
-            string pointPattern = @"[^.]*$";
-            Match match = Regex.Match(itemName, pointPattern);
-            if (match.Success)
-            {
-                // Split by capital letters. Ej: "MyBrain" -> "My", "Brain"
-                string capitalPattern = @"(?=\p{Lu})";
-                string[] result = Regex.Split(match.Value, capitalPattern);
-                // Join the words in the array with a space
-                item.text = string.Join(" ", result);
-            }
-            // TODO: Style the item using a uss class
-            item.style.color = Color.white;
-        }
-
-        #region UTILITIES
-        // Recursively, finds the brain associated to the selected item
-        private Brain GetParentBrain(int index)
-        {
-            var parentId = brainTree.GetParentIdForIndex(index);
-            // We hit the root of the tree
-            if (parentId == -1)
-            {
-                return brainTree.GetItemDataForIndex<Brain>(index);
-            }
-            DebugSelectedItemInfo(brainTree.selectedIndex);
-            // Else, set the selected item by id. This forces the tree to update the selected item
-            // thus being able to traverse the tree upwards, since its only possible to ask for the parent
-            // of an item by index i.e. you can't ask for the parent of an item by its id, neither can
-            // get the index of an item by its id, since it changes dynamically.
-            // Use selection without notify to avoid triggering the selectedItemsChanged event causing an infinite loop
-            brainTree.SetSelectionByIdWithoutNotify(new List<int>() { parentId });
-            return GetParentBrain(brainTree.selectedIndex);
-        }
-        private string PrintArray(IEnumerable<int> array)
-        {
-            string result = "[";
-            foreach (var item in array)
-            {
-                result += item + ", ";
-            }
-            result += "]";
-            return result;
-        }
-        private void DebugSelectedItemInfo(int index)
-        {
-            Debug.Log($"[Editor Window Controller] Item index: {index} " +
-                $"\nitem: {brainTree.selectedItem}" +
-                $"\nitem type: {brainTree.selectedItem.GetType()}" +
-                $"\nItem ID: {brainTree.GetIdForIndex(index)}" +
-                "\nitem children: " + PrintArray(brainTree.GetChildrenIdsForIndex(index)) +
-                $"\nitem parent's ID: {brainTree.GetParentIdForIndex(index)}" +
-                $"\nRoot element for index {index}: {brainTree.GetRootElementForIndex(index)}"
-            //$"\nitem parent's data (index): {brainTree.GetItemDataForIndex<object>(index)}" +
-            //$"\nitem parent's data (ID): {brainTree.GetItemDataForId<object>(brainTree.GetParentIdForIndex(index))}"
-            );
-        } 
-        #endregion
+        
     }
 }
