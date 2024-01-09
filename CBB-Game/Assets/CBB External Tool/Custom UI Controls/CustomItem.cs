b2e053dc61f6c593c07c54fc89fc59387cf97015
dnorambu <diego.norambuena@sansano.usm.cl>
2024-01-09 12:09:36 -0300

    [UI External] Custom Item added
    Custom UI Control used to show TreeViewItems

---
+++ CBB-Game/Assets/CBB External Tool/Custom UI Controls/CustomItem.cs
@@ -0,0 +1,35 @@
+﻿using CBB.ExternalTool;
+using UnityEngine.UIElements;
+using UnityEngine;
+namespace CBB.UI
+
+{
+    public class CustomItem : VisualElement, IDataItem
+    {
+        public new class UxmlFactory : UxmlFactory<CustomItem, UxmlTraits> { }
+
+        public Button ActionButton { get; set; }
+        public Label ItemName { get; set; }
+
+        public CustomItem()
+        {
+            var visualTree = Resources.Load<VisualTreeAsset>("Editor Mode/Button Item");
+
+            visualTree.CloneTree(this);
+
+            ActionButton = this.Q<Button>("action-button");
+            ItemName = this.Q<Label>("item-name");
+        }
+        public string GetItemName() => "+";
+        public object GetInstance() => this;
+        public void HideActionButton()
+        {
+            if(ActionButton != null)
+            {
+                ActionButton.style.display = DisplayStyle.None;
+            }
+        }
+
+    }
+
+}
