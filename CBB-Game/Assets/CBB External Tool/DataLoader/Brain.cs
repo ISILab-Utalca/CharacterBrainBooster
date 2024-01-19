92879ef0f612dcd06023ce9eb4f02825068d7166
dnorambu <diego.norambuena@sansano.usm.cl>
2024-01-19 09:15:06 -0300

    Move Brain class to its own separate file

---
+++ CBB-Game/Assets/CBB External Tool/DataLoader/Brain.cs
@@ -0,0 +1,23 @@
+﻿using Generic;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// this class is used to store the generic brain data
+/// </summary>
+[System.Serializable]
+public class Brain : IDataItem
+{
+    public string brain_ID;
+    [SerializeField, SerializeReference]
+    public List<DataGeneric> serializedActions;
+    [SerializeField, SerializeReference]
+    public List<DataGeneric> serializedSensors;
+
+    public object GetInstance() => this;
+
+    public string GetItemName() => brain_ID;
+    
+}
+
+
+
