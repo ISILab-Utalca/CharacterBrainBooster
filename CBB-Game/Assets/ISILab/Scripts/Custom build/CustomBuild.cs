43b97d83eef1a3bdd5f136425f64d99f9f9c1832
dnorambu <diego.norambuena@sansano.usm.cl>
2024-01-20 12:11:48 -0300

    Custom build scripts added
    After building the game scene, this script copies the brain file and paste them in the build folder

---
+++ CBB-Game/Assets/ISILab/Scripts/Custom build/CustomBuild.cs
@@ -0,0 +1,32 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+namespace ISILab
+{
+    class CustomBuildScripts
+    {
+        [PostProcessBuild(0)]
+        public static void BuildWindows(BuildTarget target, string pathToBuild)
+        {
+            Debug.Log("Path to build: " + pathToBuild);
+
+            // Copy all .brain files from the project folder to the build folder, alongside the built game.
+            string root = DataLoader.Path + "/Brains";
+            string[] files = System.IO.Directory.GetFiles(root, "*.brain");
+
+            // Remove the last part of pathToBuild, which is the name of the game
+            var brainsDestinationFolder = pathToBuild[..pathToBuild.LastIndexOf('/')];
+            brainsDestinationFolder += "/Brains";
+            foreach (string file in files)
+            {
+                string name = System.IO.Path.GetFileName(file);
+                string dest = System.IO.Path.Combine(brainsDestinationFolder, name);
+                System.IO.File.Copy(file, dest, true);
+            }
+            Debug.Log(files.Length + " brains copied to build folder.");
+        }
+    }
+} 
+#endif
