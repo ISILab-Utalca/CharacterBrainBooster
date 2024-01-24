a24d1d41f80c069a307cc4161e16ea16aa997bb5
dnorambu <diego.norambuena@sansano.usm.cl>
2024-01-24 18:46:32 -0300

    Constructor and interface added to Brain class
    INameable for displaying/setting its name from the UI; constructors to avoid nulls after creating the object in the UI

--- CBB-Game/Assets/_CBB/External Tool/DataLoader/Brain.cs
+++ CBB-Game/Assets/_CBB/External Tool/DataLoader/Brain.cs
@@ -1,22 +1,42 @@
-﻿using Generic;
+﻿using CBB.Api;
+using Generic;
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
 /// this class is used to store the generic brain data
 /// </summary>
 [System.Serializable]
-public class Brain : IDataItem
+public class Brain : IDataItem, INameable
 {
     public string brain_ID;
+    public string brain_Name;
     [SerializeField, SerializeReference]
     public List<DataGeneric> serializedActions;
     [SerializeField, SerializeReference]
     public List<DataGeneric> serializedSensors;
-
+    public Brain(string brain_ID, string brain_Name, List<DataGeneric> serializedActions, List<DataGeneric> serializedSensors)
+    {
+        this.brain_ID = brain_ID;
+        this.brain_Name = brain_Name;
+        this.serializedActions = serializedActions;
+        this.serializedSensors = serializedSensors;
+    }
+    
+    public Brain()
+    {
+        this.brain_ID = "";
+        this.brain_Name = "";
+        this.serializedActions = new List<DataGeneric>();
+        this.serializedSensors = new List<DataGeneric>();
+    }
     public object GetInstance() => this;
 
-    public string GetItemName() => brain_ID;
-    
+    public string GetItemName() => brain_Name;
+
+    public void SetName(string name)
+    {
+        brain_Name = name;
+    }
 }
 
 
