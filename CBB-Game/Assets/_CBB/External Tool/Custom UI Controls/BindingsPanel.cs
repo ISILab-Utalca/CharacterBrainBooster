c2d897333d42dbf64606c5a2467ee4ff0f1c594e
dnorambu <diego.norambuena@sansano.usm.cl>
2024-02-16 14:27:15 -0300

    Add method to change the dropdown display of Bindings Panel

--- CBB-Game/Assets/_CBB/External Tool/Custom UI Controls/BindingsPanel.cs
+++ CBB-Game/Assets/_CBB/External Tool/Custom UI Controls/BindingsPanel.cs
@@ -10,11 +10,19 @@ namespace CBB.ExternalTool
         public new class UxmlFactory : UxmlFactory<BindingsPanel, UxmlTraits> { }
 
         private TreeView m_bindingsTreeView;
+        private DropdownField m_agentTypes;
+
         public BindingsPanel()
         {
             var visualTree = Resources.Load<VisualTreeAsset>("Editor Mode/Bindings Panel");
             visualTree.CloneTree(this);
             m_bindingsTreeView = this.Q<TreeView>();
+            m_agentTypes = this.Q<DropdownField>();
+        }
+        public void SetAgentTypes(List<string> agentTypes)
+        {
+            m_agentTypes.Clear();
+            m_agentTypes.choices = agentTypes;
         }
     }
 }
